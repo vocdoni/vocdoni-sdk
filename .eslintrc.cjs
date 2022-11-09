3e8e23f8eb43d9a265d2f4359bba75912e9cb5bb
Òscar Casajuana <elboletaire@gmail.com>
2022-11-09 13:13:26 +0100

    Fix eslint command to work on all OS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,9 @@
 module.exports = {
-  extends: [
-    'plugin:prettier/recommended'
-  ],
+  extends: ['plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
 };
