cbd673b2b8ab54e592daf7a9d30fc0e02eac617f
Òscar Casajuana <elboletaire@gmail.com>
2022-11-14 17:37:07 +0100

    Fix undefined env vars regression caused by esbuild migration

--- esbuild.ts
+++ esbuild.ts
@@ -1,10 +1,20 @@
 import GlobalsPolyfills from '@esbuild-plugins/node-globals-polyfill'
 import NodeModulesPolyfills from '@esbuild-plugins/node-modules-polyfill'
+import de from 'dotenv'
 import esbuild from 'esbuild'
 import { readdirSync, statSync } from 'fs'
 import { join } from 'path'
 
+// read .env vars
+const opts = de.config()
+
 const watch = process.argv.slice(2, 3).pop() === '--watch'
+
+const define = {}
+
+for (const k in opts.parsed) {
+  define[`process.env.${k}`] = JSON.stringify(opts.parsed[k])
+}
 
 // Select all typescript files of src directory as entry points
 const entryPoints = readdirSync(join(process.cwd(), 'src'))
@@ -26,11 +36,13 @@ esbuild
         format: 'esm',
         platform: 'browser',
         target: ['esnext'],
+        define,
         plugins: [
           NodeModulesPolyfills(),
           GlobalsPolyfills({
             buffer: true,
             process: true,
+            define,
           }),
         ],
     })
