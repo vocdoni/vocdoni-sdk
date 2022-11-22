babc0b416ff2e6d54cf918a53cfd405aeea64763
Marc Velmer <marc1987@gmail.com>
2022-11-22 13:59:16 +0100

    Fixed metadata CID for accounts

--- src/core/account.ts
+++ src/core/account.ts
@@ -1,7 +1,6 @@
-import Hash from 'ipfs-only-hash';
 import { AccountData } from '../client';
 import { CollectFaucetTx, SetAccountTx, Tx, TxType } from '../dvote-protobuf/build/ts/vochain/vochain';
-import { Account, AccountMetadata, AccountMetadataTemplate, checkValidAccountMetadata } from '../types';
+import { Account, AccountMetadata } from '../types';
 import { TransactionCore } from './transaction';
 
 export abstract class AccountCore extends TransactionCore {
@@ -15,19 +14,19 @@ export abstract class AccountCore extends TransactionCore {
   public static generateSetAccountTransaction(
     address: string,
     account: Account,
+    cid: string,
     faucetPackage
-  ): Promise<{ tx: Uint8Array; metadata: string }> {
-    return this.prepareSetAccountData(address, account, faucetPackage).then((txData) => {
-      const setAccount = SetAccountTx.fromPartial({
-        ...txData.accountData,
-      });
-      return {
-        tx: Tx.encode({
-          payload: { $case: 'setAccount', setAccount },
-        }).finish(),
-        metadata: txData.metadata,
-      };
+  ): { tx: Uint8Array; metadata: string } {
+    const txData = this.prepareSetAccountData(address, account.generateMetadata(), cid, faucetPackage);
+    const setAccount = SetAccountTx.fromPartial({
+      ...txData.accountData,
     });
+    return {
+      tx: Tx.encode({
+        payload: { $case: 'setAccount', setAccount },
+      }).finish(),
+      metadata: txData.metadata,
+    };
   }
 
   public static generateCollectFaucetTransaction(accountData: AccountData, faucetPackage): Uint8Array {
@@ -38,48 +37,26 @@ export abstract class AccountCore extends TransactionCore {
     }).finish();
   }
 
-  private static async prepareSetAccountData(
+  private static prepareSetAccountData(
     address: string,
-    account: Account,
+    metadata: AccountMetadata,
+    cid: string,
     faucetPackage
-  ): Promise<{ metadata: string; accountData: object }> {
-    return this.generateMetadata(account).then((metadata) => {
-      return {
-        metadata: Buffer.from(JSON.stringify(metadata.metadata), 'binary').toString('base64'),
-        accountData: {
-          txtype: TxType.CREATE_ACCOUNT,
-          account: Uint8Array.from(Buffer.from(address)),
-          infoURI: 'ipfs://' + metadata.id,
-          faucetPackage: faucetPackage
-            ? {
-                payload: Uint8Array.from(Buffer.from(faucetPackage.payload, 'base64')),
-                signature: Uint8Array.from(Buffer.from(faucetPackage.signature, 'hex')),
-              }
-            : null,
-        },
-      };
-    });
-  }
-
-  private static async generateMetadata(account: Account): Promise<{ id: string; metadata: AccountMetadata }> {
-    const metadata = AccountMetadataTemplate;
-
-    metadata.languages = account.languages;
-    metadata.name = account.name;
-    metadata.description = account.description;
-    metadata.newsFeed = account.feed;
-    metadata.media = {
-      avatar: account.avatar,
-      header: account.header,
-      logo: account.logo,
+  ): { metadata: string; accountData: object } {
+    return {
+      metadata: Buffer.from(JSON.stringify(metadata), 'binary').toString('base64'),
+      accountData: {
+        txtype: TxType.CREATE_ACCOUNT,
+        account: Uint8Array.from(Buffer.from(address)),
+        infoURI: cid,
+        faucetPackage: faucetPackage
+          ? {
+              payload: Uint8Array.from(Buffer.from(faucetPackage.payload, 'base64')),
+              signature: Uint8Array.from(Buffer.from(faucetPackage.signature, 'hex')),
+            }
+          : null,
+      },
     };
-    metadata.meta = account.meta.reduce((a, v) => ({ ...a, [v.key]: v.value }), {});
-
-    checkValidAccountMetadata(metadata);
-
-    return Hash.of(JSON.stringify(metadata)).then((id) => {
-      return { id, metadata };
-    });
   }
 
   private static prepareCollectFaucetData(accountData: AccountData, faucetPackage) {
