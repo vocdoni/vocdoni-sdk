529f6e7a4a48e7488c1e06644a3bcd8041af4371
Marc Velmer <marc1987@gmail.com>
2022-11-23 13:38:47 +0100

    Added weighted election test

--- test/integration/election.test.ts
+++ test/integration/election.test.ts
@@ -1,6 +1,6 @@
 import { computePublicKey } from '@ethersproject/signing-key';
 import { Wallet } from '@ethersproject/wallet';
-import { Election, PlainCensus, VocdoniSDKClient, Vote } from '../../src';
+import { Election, PlainCensus, VocdoniSDKClient, Vote, WeightedCensus } from '../../src';
 import { delay } from '../../src/util/common';
 
 let client: VocdoniSDKClient;
@@ -122,6 +122,48 @@ describe('Election integration tests', () => {
       .then((electionData) => {
         expect(electionData.voteCount).toEqual(numVotes);
         expect(electionData.result[0][0]).toEqual(electionData.result[0][1]);
+      });
+  }, 285000);
+  it('should create an election with 100 weighted participants and each of them should vote correctly', async () => {
+    const numVotes = 100; // should be even number
+    const census = new WeightedCensus();
+
+    const participants: Wallet[] = [...new Array(numVotes)].map(() => Wallet.createRandom());
+    census.add(
+      participants.map((participant, index) => ({
+        key: participant.address,
+        weight: BigInt(index + 1),
+      }))
+    );
+
+    const election = createElection(census);
+
+    await client.createAccount();
+
+    let electionIdentifier;
+
+    await client
+      .createElection(election)
+      .then((electionId) => {
+        expect(electionId).toMatch(/^[0-9a-fA-F]{64}$/);
+        client.setElectionId(electionId);
+        electionIdentifier = electionId;
+        return delay(12000);
+      })
+      .then(() =>
+        Promise.all(
+          participants.map(async (participant, index) => {
+            client = new VocdoniSDKClient(process.env.API_URL, participant, electionIdentifier);
+            return client.submitVote(new Vote([index % 2]));
+          })
+        )
+      )
+      .then(() => client.fetchElection())
+      .then((electionData) => {
+        expect(electionData.voteCount).toEqual(numVotes);
+        expect(+electionData.result[0][0]).toBeLessThan(+electionData.result[0][1]);
+        expect(+electionData.result[0][0] + +electionData.result[0][1]).toEqual((numVotes * (numVotes + 1)) / 2);
+        expect(+electionData.result[0][0]).toEqual(+electionData.result[0][1] - numVotes / 2);
       });
   }, 285000);
 });
