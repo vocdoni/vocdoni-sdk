e4b52dfcdde5a7ecd11e65f3715569f8f54d23ff
Marc Velmer <marc1987@gmail.com>
2022-11-29 12:57:28 +0100

    Removed the `dotenv` package

--- esbuild.ts
+++ esbuild.ts
@@ -1,49 +1,33 @@
-import GlobalsPolyfills from '@esbuild-plugins/node-globals-polyfill'
-import NodeModulesPolyfills from '@esbuild-plugins/node-modules-polyfill'
-import de from 'dotenv'
-import esbuild from 'esbuild'
-import { readdirSync, statSync } from 'fs'
-import { join } from 'path'
+import GlobalsPolyfills from '@esbuild-plugins/node-globals-polyfill';
+import NodeModulesPolyfills from '@esbuild-plugins/node-modules-polyfill';
+import esbuild from 'esbuild';
+import { readdirSync, statSync } from 'fs';
+import { join } from 'path';
 
-// read .env vars
-const opts = de.config()
-
-const watch = process.argv.slice(2, 3).pop() === '--watch'
-
-const define = {}
-
-for (const k in opts.parsed) {
-  define[`process.env.${k}`] = JSON.stringify(opts.parsed[k])
-}
+const watch = process.argv.slice(2, 3).pop() === '--watch';
 
 // Select all typescript files of src directory as entry points
 const entryPoints = readdirSync(join(process.cwd(), 'src'))
-  .filter(
-    (file) =>
-      file.endsWith('.ts') &&
-      statSync(join(process.cwd(), 'src', file)).isFile()
-  )
-  .map((file) => `src/${file}`)
+  .filter((file) => file.endsWith('.ts') && statSync(join(process.cwd(), 'src', file)).isFile())
+  .map((file) => `src/${file}`);
 
 esbuild
-    .build({
-        entryPoints,
-        outdir: 'dist',
-        bundle: true,
-        sourcemap: true,
-        minify: true,
-        watch,
-        format: 'esm',
-        platform: 'browser',
-        target: ['esnext'],
-        define,
-        plugins: [
-          NodeModulesPolyfills(),
-          GlobalsPolyfills({
-            buffer: true,
-            process: true,
-            define,
-          }),
-        ],
-    })
-    .catch(() => process.exit(1))
+  .build({
+    entryPoints,
+    outdir: 'dist',
+    bundle: true,
+    sourcemap: true,
+    minify: true,
+    watch,
+    format: 'esm',
+    platform: 'browser',
+    target: ['esnext'],
+    plugins: [
+      NodeModulesPolyfills(),
+      GlobalsPolyfills({
+        buffer: true,
+        process: true,
+      }),
+    ],
+  })
+  .catch(() => process.exit(1));
