65c5b0106fca18d4c5eaa4a16c5b02ac9d4c13ed
Roger Carhuatocto <chilcano@intix.info>
2022-12-06 16:47:22 +0100

    DOPS-405 sdk docs are generated and uploaded to developer-portal repo (#30)
    Signed-off-by: emmdim <emmdim@users.noreply.github.com>
    Co-authored-by: selankon <selankon@selankon.xyz>
    Co-authored-by: emmdim <emmdim@users.noreply.github.com>

---
+++ docs/README.md
@@ -0,0 +1,23 @@
+# Vocdoni SDK Docs
+
+Here you can generate the Vocdoni SDK documentation from the JSDocs annotations on the source code. It uses the package
+[`jsdoc-to-mdx`](https://github.com/naver/jsdoc-to-mdx) that untherneath uses the well known `jsdoc-to-markdown` library.
+
+This library export the JSDocs to `.mdx` format, which is compatible with docusaurus instances. 
+
+### How to generate the docs
+
+Run:
+
+```bash
+npm install
+```
+
+Then you can just run the npm script:
+
+```bash
+npm run build
+```
+
+This will generate a `docs_sdk` folder with all generated documentation. It also copies de `../README.md` into 
+`docs_sdk/sdk.md` to use it as docusaurus main page.
---
+++ docs/docs_sdk/sidebars-api.js
@@ -0,0 +1,45 @@
+module.exports = {
+  "api": [
+    {
+      "type": "category",
+      "label": "Class",
+      "items": [
+        "sdk/AccountAPI",
+        "sdk/CensusAPI",
+        "sdk/ChainAPI",
+        "sdk/ElectionAPI",
+        "sdk/FaucetAPI",
+        "sdk/FileAPI",
+        "sdk/VoteAPI",
+        "sdk/WalletAPI",
+        "sdk/AccountCore",
+        "sdk/ElectionCore",
+        "sdk/TransactionCore",
+        "sdk/VoteCore",
+        "sdk/Account",
+        "sdk/Census",
+        "sdk/OffchainCensus",
+        "sdk/PlainCensus",
+        "sdk/PublishedCensus",
+        "sdk/WeightedCensus",
+        "sdk/Election",
+        "sdk/Vote",
+        "sdk/Asymmetric",
+        "sdk/Signing"
+      ]
+    },
+    {
+      "type": "category",
+      "label": "Global",
+      "items": [
+        "sdk/AccountMetadata",
+        "sdk/ElectionMetadata",
+        "sdk/checkValidAccountMetadata",
+        "sdk/checkValidElectionMetadata",
+        "sdk/getHex",
+        "sdk/allSettled",
+        "sdk/promiseAny"
+      ]
+    }
+  ]
+};
