7dc342e047edc88a7a5a50b04351a0be201ff452
Ivan Vergés <ivan@pokecode.net>
2023-02-17 18:01:49 +0100

    Fixed metadata encoding and new test

--- test/integration/other.test.ts
+++ test/integration/other.test.ts
@@ -9,4 +9,17 @@ describe('Other tests', () => {
     const cid = await client.calculateCID(b64Metadata);
     expect(cid).toEqual('ipfs://bagaaieratw2qwvf4mt7qh4mjucqol6542q2t655q3scrx4gc7xtjlrihompq');
   });
+  it('should calculate the correct IPFS hash with special characters', async () => {
+    const client = new VocdoniSDKClient(clientParams());
+    const election = ElectionMetadataTemplate;
+    election.title = {
+      default: 'Això és un títol',
+    };
+    election.description = {
+      default: 'こんにちは世界',
+    };
+    const b64Metadata = Buffer.from(JSON.stringify(election), 'utf8').toString('base64');
+    const cid = await client.calculateCID(b64Metadata);
+    expect(cid).toEqual('ipfs://bagaaierapxv6hx7misrwc6ydjuaae7wxwsvucvtqzjj3wusbmpg6vdav2e5a');
+  });
 });
