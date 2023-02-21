0a0c64d2b921b62aaad17115dd4cf9d9e5a60c16
Ivan Vergés <ivan@pokecode.net>
2023-02-21 15:07:03 +0100

    change binary by utf8

--- test/integration/other.test.ts
+++ test/integration/other.test.ts
@@ -5,7 +5,7 @@ import { clientParams } from './util/client.params';
 describe('Other tests', () => {
   it('should calculate the correct IPFS hash', async () => {
     const client = new VocdoniSDKClient(clientParams());
-    const b64Metadata = Buffer.from(JSON.stringify(ElectionMetadataTemplate), 'binary').toString('base64');
+    const b64Metadata = Buffer.from(JSON.stringify(ElectionMetadataTemplate), 'utf8').toString('base64');
     const cid = await client.calculateCID(b64Metadata);
     expect(cid).toEqual('ipfs://bagaaieratw2qwvf4mt7qh4mjucqol6542q2t655q3scrx4gc7xtjlrihompq');
   });
