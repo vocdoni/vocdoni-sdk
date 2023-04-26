1eba3ff64122272686509d246303e970b9264ded
Marc Velmer <marc1987@gmail.com>
2023-04-26 16:23:07 +0200

    Added `getStrategiesList`, `getStrategies` and `getStrategy`

---
+++ src/api/census3/strategies.ts
@@ -0,0 +1,93 @@
+import axios from 'axios';
+import { Census3API } from './api';
+
+enum Census3StrategiesAPIMethods {
+  LIST = '/strategies/page/{page}',
+  LIST_BY_TOKEN = '/strategies/token/{id}',
+  STRATEGY = '/strategies/{id}',
+}
+
+export interface ICensus3StrategiesListResponse {
+  /**
+   * The list of the strategies identifiers
+   */
+  strategies: Array<number>;
+}
+
+export interface ICensus3StrategyResponse {
+  /**
+   * The list of tokens
+   */
+  tokens: Array<ICensus3StrategyToken>;
+
+  /**
+   * The predicate
+   */
+  predicate: string;
+}
+
+export interface ICensus3StrategyToken {
+  /**
+   * The id (address) of the token.
+   */
+  id: string;
+
+  /**
+   * The name of the token.
+   */
+  name: string;
+
+  /**
+   * The minimum balance.
+   */
+  minimum_balance: string;
+
+  /**
+   * The method used for checking balances.
+   */
+  method: string;
+}
+
+export abstract class Census3StrategiesAPI extends Census3API {
+  /**
+   * Cannot be constructed.
+   */
+  private constructor() {
+    super();
+  }
+
+  /**
+   * Fetches list of strategies
+   *
+   * @param {string} url API endpoint URL
+   * @param {number} page The page number
+   * @param {string} token The token id or address
+   * @returns {Promise<ICensus3StrategiesListResponse>}
+   */
+  public static list(url: string, page: number = 0, token?: string): Promise<ICensus3StrategiesListResponse> {
+    if (token) {
+      return axios
+        .get<ICensus3StrategiesListResponse>(url + Census3StrategiesAPIMethods.LIST_BY_TOKEN.replace('{id}', token))
+        .then((response) => response.data)
+        .catch(this.isApiError);
+    }
+    return axios
+      .get<ICensus3StrategiesListResponse>(url + Census3StrategiesAPIMethods.LIST.replace('{page}', String(page)))
+      .then((response) => response.data)
+      .catch(this.isApiError);
+  }
+
+  /**
+   * Returns the information of the strategy
+   *
+   * @param {string} url API endpoint URL
+   * @param {number} id The identifier of the strategy
+   * @returns {Promise<ICensus3StrategyResponse>}
+   */
+  public static strategy(url: string, id: number): Promise<ICensus3StrategyResponse> {
+    return axios
+      .get<ICensus3StrategyResponse>(url + Census3StrategiesAPIMethods.STRATEGY.replace('{id}', String(id)))
+      .then((response) => response.data)
+      .catch(this.isApiError);
+  }
+}
