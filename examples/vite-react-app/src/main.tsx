cd984a6443825e75dea4b748a754f47a522e8492
Òscar Casajuana <elboletaire@gmail.com>
2023-07-18 13:27:18 +0200

    Fix examples

--- examples/vite-react-app/src/main.tsx
+++ examples/vite-react-app/src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-window.Buffer = window.Buffer || import('buffer').Buffer
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
