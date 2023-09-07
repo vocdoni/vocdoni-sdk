bd55a0cb30c19acbf677fc7b540bc238f8bc0545
Marc Velmer <marc1987@gmail.com>
2023-09-08 01:38:20 +0200

    Fixed CID API tests

--- test/integration/other.test.ts
+++ test/integration/other.test.ts
@@ -5,7 +5,7 @@ import { clientParams } from './util/client.params';
 describe('Other tests', () => {
   it('should calculate the correct IPFS hash', async () => {
     const client = new VocdoniSDKClient(clientParams());
-    const b64Metadata = Buffer.from(JSON.stringify(ElectionMetadataTemplate), 'utf8').toString('base64');
+    const b64Metadata = JSON.stringify(ElectionMetadataTemplate);
     const cid = await client.calculateCID(b64Metadata);
     expect(cid).toEqual('ipfs://bafybeigs3vmszvbzvhheixhgkynbc57csmga6ayhwephtyganed4xkanyi');
   });
@@ -18,7 +18,7 @@ describe('Other tests', () => {
     election.description = {
       default: 'こんにちは世界',
     };
-    const b64Metadata = Buffer.from(JSON.stringify(election), 'utf8').toString('base64');
+    const b64Metadata = JSON.stringify(election);
     const cid = await client.calculateCID(b64Metadata);
     expect(cid).toEqual('ipfs://bafybeics2rrjzgnlfliplny2l4ke7jxa3keq6oqlbg5op7pmubvz7sp5ga');
   });
