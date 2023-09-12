6a9277079970305d8a76cc7bd8bb44a95ca18bb9
Marc Velmer <marc1987@gmail.com>
2023-09-12 20:16:11 +0200

    Ensuring proof `value` handled as hex for anonymous circuits inputs.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Ensuring proof `value` handled as hex for anonymous circuits inputs.
+
 ### Added
 
 - Census3 supported chains information.
--- test/integration/zk.test.ts
+++ test/integration/zk.test.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 import { clientParams } from './util/client.params';
-import { Election, PlainCensus, VocdoniSDKClient, Vote } from '../../src';
+import { Election, PlainCensus, WeightedCensus, VocdoniSDKClient, Vote } from '../../src';
 import { Wallet } from '@ethersproject/wallet';
 // @ts-ignore
 import { waitForElectionReady } from './util/client.utils';
@@ -94,6 +94,85 @@ describe('zkSNARK test', () => {
       .then(() => {
         const vote = new Vote([1]);
         return client.submitVote(vote);
+      })
+      .then(() => client.fetchElection())
+      .then((election) => {
+        expect(election.electionType.anonymous).toBeTruthy();
+        expect(election.voteCount).toEqual(3);
+        expect(election.results[0][0]).toEqual('2');
+        expect(election.results[0][1]).toEqual('1');
+        expect(election.census.size).toEqual(3);
+        expect(election.census.weight).toEqual(BigInt(3));
+      });
+  }, 285000);
+  it('should create a weighted anonymous election and vote successfully', async () => {
+    const census = new WeightedCensus();
+    const voter1 = Wallet.createRandom();
+    const voter2 = Wallet.createRandom();
+    // User that votes with account with SIK
+    census.add({
+      key: (client.wallet as Wallet).address,
+      weight: 12n,
+    });
+    // User that votes and has no account
+    census.add({
+      key: voter1.address,
+      weight: 120n,
+    });
+    // User that votes with account without SIK
+    census.add({
+      key: voter2.address,
+      weight: 1200n,
+    });
+
+    const election = createElection(census, {
+      anonymous: true,
+    });
+
+    await client.createAccount({
+      sik: true,
+      password: 'password123',
+    });
+
+    await client
+      .createElection(election)
+      .then((electionId) => {
+        expect(electionId).toMatch(/^[0-9a-fA-F]{64}$/);
+        client.setElectionId(electionId);
+        return client.fetchElection();
+      })
+      .then((publishedElection) => {
+        expect(publishedElection.electionType.anonymous).toBeTruthy();
+        return waitForElectionReady(client, publishedElection.id);
+      })
+      .then(async () => {
+        await expect(async () => {
+          await client.submitVote(new Vote([0]));
+        }).rejects.toThrow();
+        const vote = new AnonymousVote([0], 'password123');
+        return client.submitVote(vote);
+      })
+      .then(() => {
+        client.wallet = voter1;
+        const vote = new AnonymousVote([0], 'password456');
+        return client.submitVote(vote);
+      })
+      .then(() => {
+        client.wallet = voter2;
+        return client.createAccount({ sik: false });
+      })
+      .then(() => {
+        const vote = new Vote([1]);
+        return client.submitVote(vote);
+      })
+      .then(() => client.fetchElection())
+      .then((election) => {
+        expect(election.electionType.anonymous).toBeTruthy();
+        expect(election.voteCount).toEqual(3);
+        expect(election.results[0][0]).toEqual('132');
+        expect(election.results[0][1]).toEqual('1200');
+        expect(election.census.size).toEqual(3);
+        expect(election.census.weight).toEqual(BigInt(12n + 120n + 1200n));
       });
   }, 285000);
   it('should create an anonymous election with 12 participants and each of them should vote correctly', async () => {
