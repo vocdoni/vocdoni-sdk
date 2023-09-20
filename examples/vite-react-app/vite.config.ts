6ef922caf9f73fea4ffee3bfdac19075ad2a5a49
Marc Velmer <marc1987@gmail.com>
2023-09-20 17:05:32 +0200

    Added proper config for deploying vite example to GH pages

--- examples/vite-react-app/vite.config.ts
+++ examples/vite-react-app/vite.config.ts
@@ -2,8 +2,15 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+const outDir = process.env.BUILD_PATH
+const base = process.env.BASE_URL || ''
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
+  build: {
+    outDir,
+  },
   plugins: [react()],
   define: {
     global: "window"
