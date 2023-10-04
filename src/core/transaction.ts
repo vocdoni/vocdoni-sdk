2311b61aee4ec1010122986c7969a1b151f7857c
Marc Velmer <marc1987@gmail.com>
2023-10-04 13:42:10 +0200

    Account new signatures finished

--- src/core/transaction.ts
+++ src/core/transaction.ts
@@ -4,6 +4,7 @@ import { SignedTx } from '@vocdoni/proto/vochain';
 import { Buffer } from 'buffer';
 import { strip0x } from '../util/common';
 import { Signing } from '../util/signing';
+import { keccak256 } from '@ethersproject/keccak256';
 
 export abstract class TransactionCore {
   /**
@@ -13,13 +14,17 @@ export abstract class TransactionCore {
 
   public static async signTransaction(
     tx: Uint8Array,
-    chainId: string,
+    payload: string,
     walletOrSigner: Wallet | Signer
   ): Promise<string> {
-    return Signing.signTransaction(tx, chainId, walletOrSigner).then((hexSignature) => {
+    return Signing.signTransaction(payload, walletOrSigner).then((hexSignature) => {
       const signature = new Uint8Array(Buffer.from(strip0x(hexSignature), 'hex'));
       const signedTx = SignedTx.encode({ tx, signature }).finish();
       return Buffer.from(signedTx).toString('base64');
     });
+  }
+
+  public static hashTransaction(tx: Uint8Array): string {
+    return strip0x(keccak256(tx));
   }
 }
