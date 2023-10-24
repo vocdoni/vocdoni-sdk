d8f9d75ced79a48044b50b85322a4e90367ab103
Marc Velmer <marc1987@gmail.com>
2023-10-24 12:59:00 +0200

    Added `setFaucetURL` in common test helper functions

--- test/integration/util/client.params.ts
+++ test/integration/util/client.params.ts
@@ -1,8 +1,14 @@
 import { ClientOptions, EnvOptions } from '../../../src';
 import { Wallet } from '@ethersproject/wallet';
+import { VocdoniSDKClient } from '@vocdoni/sdk';
 
 export const clientParams = (wallet?: Wallet): ClientOptions => ({
   env: EnvOptions.DEV,
   api_url: process.env.API_URL,
   wallet: wallet ?? Wallet.createRandom(),
 });
+
+export const setFaucetURL = (client: VocdoniSDKClient): VocdoniSDKClient => {
+  client.faucetService.url = process.env.FAUCET_URL ?? client.faucetService.url;
+  return client;
+};
