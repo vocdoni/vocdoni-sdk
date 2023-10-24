b7beab14196655244479c944d7a877fd41f17fc5
Marc Velmer <marc1987@gmail.com>
2023-10-24 12:42:25 +0200

    Removed one useless test from accounts

--- test/integration/account.test.ts
+++ test/integration/account.test.ts
@@ -36,15 +36,6 @@ describe('Account integration tests', () => {
       await client.collectFaucetTokens();
     }).rejects;
   }, 75000);
-  it('should bootstrap a new account and fetch tokens from raw faucet package', async () => {
-    const accountInfo = await client.createAccount();
-    expect(accountInfo.balance).toBeGreaterThan(0);
-
-    await client
-      .fetchFaucetPayload()
-      .then((faucetPackage) => client.collectFaucetTokens(faucetPackage))
-      .then((finalAccountInfo) => expect(finalAccountInfo.balance).toBeGreaterThan(accountInfo.balance));
-  }, 75000);
   it('should bootstrap a new account and do nothing when creating it twice', async () => {
     const accountInfo = await client.createAccount();
     const accountInfoAfter = await client.createAccount();
