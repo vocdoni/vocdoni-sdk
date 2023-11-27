d804c6502d1dbbc42c5fa337ed8445b2372387be
Marc Velmer <marc1987@gmail.com>
2023-11-27 13:49:58 +0100

    Fixed archived elections for accepting censuses without URI

--- src/types/census/archived.ts
+++ src/types/census/archived.ts
@@ -1,4 +1,4 @@
-import { Census } from './census';
+import { Census, CensusType } from './census';
 import invariant from 'tiny-invariant';
 
 /**
@@ -10,14 +10,12 @@ export class ArchivedCensus extends Census {
    *
    * @param censusId The id of the census
    * @param censusURI The URI of the census
+   * @param type The type of the census
+   * @param size The size of the census
+   * @param weight The weight of the census
    */
-  public constructor(censusId: string, censusURI: string) {
+  public constructor(censusId: string, censusURI?: string, type?: CensusType, size?: number, weight?: bigint) {
     invariant(/^(0x)?[0-9a-fA-F]+$/.test(censusId), 'Census identifier is missing or invalid');
-    try {
-      new URL(censusURI);
-    } catch (_) {
-      invariant(false, 'Census URI is missing or invalid');
-    }
-    super(censusId, censusURI);
+    super(censusId, censusURI, type, size, weight);
   }
 }
