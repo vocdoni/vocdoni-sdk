ac38058d244838f979fe27d3bc32fd5543c76246
NateWilliams2 <nathanielj.williams02@gmail.com>
2024-03-11 16:36:06 -0400

    Examples: update tutorial example

--- examples/tutorial/src/client.ts
+++ examples/tutorial/src/client.ts
@@ -1,13 +1,12 @@
-import { EnvOptions, VocdoniSDKClient } from '@vocdoni/sdk'
-import { Wallet } from '@ethersproject/wallet'
+import { EnvOptions, VocdoniSDKClient } from '@vocdoni/sdk';
+import { Wallet } from '@ethersproject/wallet';
 
 export const getDefaultClient = () => {
-  const wallet = Wallet.createRandom()
+  const wallet = Wallet.createRandom();
   const client = new VocdoniSDKClient({
     env: EnvOptions.STG,
-    api_url: 'https://api-stg.vocdoni.net/v2',
-    wallet: wallet
-  })
+    wallet: wallet,
+  });
 
-  return { wallet, client }
-}
+  return { wallet, client };
+};
