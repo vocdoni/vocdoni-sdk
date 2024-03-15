bf28fda834de388d214a656dafa5e15afdfea694
NateWilliams2 <nathanielj.williams02@gmail.com>
2024-03-15 17:20:50 -0400

    Format example code

--- examples/tutorial/src/census.ts
+++ examples/tutorial/src/census.ts
@@ -1,15 +1,14 @@
-import { PlainCensus } from '@vocdoni/sdk'
-import { Wallet } from '@ethersproject/wallet'
+import { PlainCensus } from '@vocdoni/sdk';
+import { Wallet } from '@ethersproject/wallet';
 
 export async function createCensus () {
-  const census = new PlainCensus()
-
+  const census = new PlainCensus();
   // Create a list of random voters, register them to the census
-  let voters: Wallet[] = []
+  let voters: Wallet[] = [];
   for (let i = 0; i < 5; i++) {
-    voters.push(Wallet.createRandom())
-    census.add(await voters[i].getAddress())
+    voters.push(Wallet.createRandom());
+    census.add(await voters[i].getAddress());
   }
 
-  return { census, voters }
+  return { census, voters };
 }
